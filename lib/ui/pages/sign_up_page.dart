62100e6c1db3e9f8e791dc6d591a6c85cf97ac9d
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-09-23 16:20:17 +0800

    sign up page done

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -19,9 +19,8 @@ class SignUpPage extends StatelessWidget {
 
     // Input Section Widget
     Widget inputSection() {
-
-      // Email Widget
-      Widget emailInput() {
+      // Full Name Widget
+      Widget nameInput() {
         return Container(
           margin: const EdgeInsets.only(bottom: 20),
           child: Column(
@@ -32,20 +31,118 @@ class SignUpPage extends StatelessWidget {
               TextFormField(
                 cursorColor: kBlackColor,
                 decoration: InputDecoration(
-                  hintText: 'John Doe',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(defaultRadius),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(defaultRadius),
-                    borderSide: BorderSide(color: kPrimaryColor)
-                  )
-                ),
+                    hintText: 'John Doe',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        borderSide: BorderSide(color: kPrimaryColor))),
+              ),
+            ],
+          ),
+        );
+      } // nameInput
+
+      // Email Widget
+      Widget emailInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Email'),
+              const SizedBox(height: 6),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                    hintText: 'johndoe@email.com',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        borderSide: BorderSide(color: kPrimaryColor))),
               ),
             ],
           ),
         );
       } // emailInput
+
+      // Password Widget
+      Widget passwordInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Password'),
+              const SizedBox(height: 6),
+              TextFormField(
+                obscureText: true,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        borderSide: BorderSide(color: kPrimaryColor))),
+              ),
+            ],
+          ),
+        );
+      } // passwordInput
+
+      // Hobby Widget
+      Widget hobbyInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Hobby'),
+              const SizedBox(height: 6),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                    hintText: 'Your Hobby',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        borderSide: BorderSide(color: kPrimaryColor))),
+              ),
+            ],
+          ),
+        );
+      } // hobbyInput
+
+      // Submit Button Widget
+      Widget submitButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(defaultRadius),
+              ),
+            ),
+            child: Text(
+              'Get Started',
+              style: whiteTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: medium,
+              ),
+            ),
+          ),
+        );
+      }
 
       return Container(
         margin: const EdgeInsets.only(top: 30),
@@ -58,7 +155,33 @@ class SignUpPage extends StatelessWidget {
           borderRadius: BorderRadius.circular(defaultRadius),
         ),
         child: Column(
-          children: [emailInput()],
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            hobbyInput(),
+            submitButton(),
+          ],
+        ),
+      );
+    }
+
+    // TnC Widget
+
+    Widget tncButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(
+          top: 50,
+          bottom: 73,
+        ),
+        child: Text(
+          'Terms and Conditions',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+            decoration: TextDecoration.underline,
+          ),
         ),
       );
     }
@@ -73,6 +196,7 @@ class SignUpPage extends StatelessWidget {
           children: [
             title(),
             inputSection(),
+            tncButton()
           ],
         ),
       ),
