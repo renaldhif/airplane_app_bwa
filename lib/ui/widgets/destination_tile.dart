6bc80a4d5a50acddfc85bb29c6a290ef9f4fc624
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-10-01 00:06:48 +0800

    adding destination tile

---
+++ lib/ui/widgets/destination_tile.dart
@@ -0,0 +1,92 @@
+import 'package:airplane_app_bwa/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class DestinationTile extends StatelessWidget {
+  
+  final String nameNewDestination, city, imageUrl;
+  final double rating;
+
+  const DestinationTile({
+    super.key,
+    required this.nameNewDestination,
+    required this.city,
+    required this.imageUrl,
+    this.rating = 0.0,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 16),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: kWhiteColor,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Row(
+        children: [
+          // Image
+          Container(
+            width: 70,
+            height: 70,
+            margin: const EdgeInsets.only(right: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              image: DecorationImage(
+                fit: BoxFit.cover, // to fit the image to the container size
+                image: AssetImage(imageUrl),
+              ),
+            ),
+          ),
+
+          // Content
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  nameNewDestination,
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  city,
+                  style: greyTextStyle.copyWith(
+                    fontWeight: light,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Rating
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                margin: const EdgeInsets.only(right: 2),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/icon_star.png'),
+                  ),
+                ),
+              ),
+              Text(
+                rating.toString(),
+                style: blackTextStyle.copyWith(fontWeight: medium),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
