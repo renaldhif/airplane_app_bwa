f4b9166d3cd297fc2c283d1696bb132b84a7f328
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-10-01 03:31:01 +0800

    adding gesture detector

--- lib/ui/widgets/destination_card.dart
+++ lib/ui/widgets/destination_card.dart
@@ -1,5 +1,7 @@
+import 'package:airplane_app_bwa/ui/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane_app_bwa/shared/theme.dart';
+import 'package:airplane_app_bwa/ui/pages/detail_page.dart';
 
 class DestinationCard extends StatelessWidget {
   final String destinationName, city, imageUrl;
@@ -11,95 +13,107 @@ class DestinationCard extends StatelessWidget {
     required this.city,
     required this.imageUrl,
     this.rating = 0.0,
-    });
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 323,
-      margin: EdgeInsets.only(left: defaultMargin),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(18),
-        color: kWhiteColor,
-      ),
+    // Gesture Detector means that the widget can be pressed 
+    // (in this case, navigate to the detail page)
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const DetailPage(),
+          ),
+        );
+      },
+      child: Container(
+        width: 200,
+        height: 323,
+        margin: EdgeInsets.only(left: defaultMargin),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          color: kWhiteColor,
+        ),
 
-      // Image Destination
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 180,
-            height: 220,
-            margin: const EdgeInsets.only(bottom: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              image: DecorationImage(
-                image: AssetImage(imageUrl),
-              ),
-            ),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                width: 55,
-                height: 30,
-                decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(18),
-                  ),
+        // Image Destination
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 180,
+              height: 220,
+              margin: const EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                image: DecorationImage(
+                  image: AssetImage(imageUrl),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 20,
-                      width: 20,
-                      margin: const EdgeInsets.only(right: 2),
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/icon_star.png'),
+              ),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  width: 55,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: kWhiteColor,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(18),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 20,
+                        width: 20,
+                        margin: const EdgeInsets.only(right: 2),
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icon_star.png'),
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      rating.toString(),
-                      style: blackTextStyle.copyWith(
-                        fontSize: 13,
-                        fontWeight: medium,
+                      Text(
+                        rating.toString(),
+                        style: blackTextStyle.copyWith(
+                          fontSize: 13,
+                          fontWeight: medium,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  destinationName,
-                  style: blackTextStyle.copyWith(
-                    fontSize: 18,
-                    fontWeight: medium,
+            Container(
+              margin: const EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    destinationName,
+                    style: blackTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: medium,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  city,
-                  style: greyTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: light,
+                  const SizedBox(height: 5),
+                  Text(
+                    city,
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: light,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/ui/widgets/destination_tile.dart
+++ lib/ui/widgets/destination_tile.dart
@@ -1,4 +1,5 @@
 import 'package:airplane_app_bwa/shared/theme.dart';
+import 'package:airplane_app_bwa/ui/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 
 class DestinationTile extends StatelessWidget {
@@ -16,76 +17,86 @@ class DestinationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 16),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: kWhiteColor,
-        borderRadius: BorderRadius.circular(18),
-      ),
-      child: Row(
-        children: [
-          // Image
-          Container(
-            width: 70,
-            height: 70,
-            margin: const EdgeInsets.only(right: 16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              image: DecorationImage(
-                fit: BoxFit.cover, // to fit the image to the container size
-                image: AssetImage(imageUrl),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(
+            builder: (context) => const DetailPage(),
+          ),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 16),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Row(
+          children: [
+            // Image
+            Container(
+              width: 70,
+              height: 70,
+              margin: const EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                image: DecorationImage(
+                  fit: BoxFit.cover, // to fit the image to the container size
+                  image: AssetImage(imageUrl),
+                ),
               ),
             ),
-          ),
-
-          // Content
-          Expanded(
-            child: Column(
+    
+            // Content
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    nameNewDestination,
+                    style: blackTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    city,
+                    style: greyTextStyle.copyWith(
+                      fontWeight: light,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    
+            // Rating
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  nameNewDestination,
-                  style: blackTextStyle.copyWith(
-                    fontSize: 18,
-                    fontWeight: medium,
+                Container(
+                  width: 20,
+                  height: 20,
+                  margin: const EdgeInsets.only(right: 2),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icon_star.png'),
+                    ),
                   ),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
                 Text(
-                  city,
-                  style: greyTextStyle.copyWith(
-                    fontWeight: light,
-                  ),
+                  rating.toString(),
+                  style: blackTextStyle.copyWith(fontWeight: medium),
                 ),
               ],
             ),
-          ),
-
-          // Rating
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                margin: const EdgeInsets.only(right: 2),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/icon_star.png'),
-                  ),
-                ),
-              ),
-              Text(
-                rating.toString(),
-                style: blackTextStyle.copyWith(fontWeight: medium),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
