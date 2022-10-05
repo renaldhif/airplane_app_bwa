136e6093a554d120ee8e60a57abd7bfae9481fd6
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-10-06 02:21:26 +0800

    adding seat item from choose seat page

---
+++ lib/ui/widgets/seat_item.dart
@@ -0,0 +1,87 @@
+import 'package:airplane_app_bwa/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class SeatItem extends StatelessWidget {
+  final int status;
+  //! status =
+  //! 0 (available),
+  //! 1 (unavailable),
+  //! 2 (selected)
+
+  const SeatItem({
+    super.key,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    backgroundColor() {
+      switch (status) {
+        case 0:
+          return kAvailableColor;
+
+        case 1:
+          return kPrimaryColor;
+
+        case 2:
+          return kUnavailableColor;
+
+        default:
+          return kUnavailableColor;
+      }
+    }
+
+    borderColor(){
+      switch (status) {
+        case 0:
+          return kPrimaryColor;
+
+        case 1:
+          return kPrimaryColor;
+
+        case 2:
+          return kUnavailableColor;
+
+        default:
+          return kUnavailableColor;
+      }
+    }
+
+    fillChild(){
+      switch (status) {
+        case 0:
+          return const SizedBox();
+
+        case 1:
+          return Center(
+            child: Text(
+              'YOU',
+              style: whiteTextStyle.copyWith(
+                fontWeight: semiBold,
+              ),
+            ),
+          );
+
+        case 2:
+          return const SizedBox();
+
+        default:
+          return const SizedBox();
+      }
+    }
+
+    return Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        color: backgroundColor(),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: borderColor(),
+          width: 2,
+        )
+      ),
+      child: fillChild()
+    );
+  }
+}
