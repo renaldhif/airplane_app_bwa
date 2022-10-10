5ac51a8c9e22214f9b465d54348ce588a32e186f
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-10-11 03:15:03 +0800

    adding switch case currentindex to botnav

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,15 +1,31 @@
+import 'package:airplane_app_bwa/cubit/page_cubit.dart';
 import 'package:airplane_app_bwa/ui/pages/home_page.dart';
+import 'package:airplane_app_bwa/ui/pages/setting_page.dart';
+import 'package:airplane_app_bwa/ui/pages/transaction_page.dart';
+import 'package:airplane_app_bwa/ui/pages/wallet_page.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane_app_bwa/shared/theme.dart';
 import 'package:airplane_app_bwa/ui/widgets/custom_botnav_items.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return const HomePage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingPage();
+        default:
+          return HomePage();
+      }
     }
 
     // Custom Bottom Navigation Bar Widget
@@ -33,22 +49,25 @@ class MainPage extends StatelessWidget {
             children: const [
               // Home
               CustomBotNavItem(
+                index: 0,
                 imageUrl: 'assets/icon_home.png',
-                isSelected: true,
               ),
 
               // Transaction
               CustomBotNavItem(
+                index: 1,
                 imageUrl: 'assets/icon_transaction.png',
               ),
 
               // Credit Card
               CustomBotNavItem(
+                index: 2,
                 imageUrl: 'assets/icon_creditcard.png',
               ),
 
               // Settings
               CustomBotNavItem(
+                index: 3,
                 imageUrl: 'assets/icon_setting.png',
               ),
             ],
@@ -57,13 +76,17 @@ class MainPage extends StatelessWidget {
       );
     } // customBotNav Widget
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          buildContent(),
-          customBotNav(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBotNav(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
