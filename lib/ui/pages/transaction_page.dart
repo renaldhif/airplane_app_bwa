67aa2479481772b09b06548f1b208b78221f3132
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-10-11 03:15:21 +0800

    adding transaction page

---
+++ lib/ui/pages/transaction_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TransactionPage extends StatelessWidget {
+  
+  const TransactionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Transaction Page'),
+    );
+  }
+}
